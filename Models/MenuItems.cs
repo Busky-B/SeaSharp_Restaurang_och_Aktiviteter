03e9197c20b9fc6a85d2a06f6f723ef1abd6a921
Tobias <72868238+Busky-B@users.noreply.github.com>
2021-02-12 19:24:16 +0100

    skapat nya MenuItems

---
+++ Models/MenuItems.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeaSharp_Restaurang_och_Aktiviteter.Models 
+{
+
+
+	// Ny Modell för MenuOptions => MenuItems
+	public class MenuItems
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Allergies { get; set; }
+        public decimal Price { get; set; }
+
+		public int MenuCategoryId { get; set; }
+		public MenuCategory MenuCategory{ get; set; }
+	}
+}
