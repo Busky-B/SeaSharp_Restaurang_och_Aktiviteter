aae37d113a0c056169a8a03e874353180980eb8d
antno <antno@LAPTOP-9U0B3ID2>
2021-03-01 12:51:27 +0100

    restinfo

---
+++ Migrations/20210301115037_rest_info.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SeaSharp_Restaurang_och_Aktiviteter.Migrations
+{
+    public partial class rest_info : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RestaurantInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Open = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Closed = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RestaurantInfo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RestaurantInfo");
+        }
+    }
+}
